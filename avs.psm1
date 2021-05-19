1755d8f73187c82d2bc9a6914aa3d620817a58b8
josprou <34846208+josprou@users.noreply.github.com>
2021-05-19 11:39:24 +0200

    Update avs.psm1

--- avs.psm1
+++ avs.psm1
@@ -1,7 +1,9 @@
 function avs{
   $Names = Get-CimInstance -Namespace root/SecurityCenter2 -ClassName AntivirusProduct | Select displayName 
   if($Names -ne $null){
-    $global:execute = $Names
+    foreach($n in $Names){
+      $global:execute += "$n `n"
+    }    
   }else{
     $global:execute = "No se detectan servicios antivirus"
   }
