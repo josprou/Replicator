3e0ceb406e287a521279c76d2c477aafc5a53a21
josprou <34846208+josprou@users.noreply.github.com>
2023-05-19 03:56:08 +0200

    Update edge.ps1

--- modules/edge.ps1
+++ modules/edge.ps1
@@ -1,19 +1,21 @@
-$Folders = Get-ChildItem -Directory "C:\Users" | Where-Object {($_.Name -notlike "Public") -and ($_.Name -notlike "Administrador")}
-$Out = ''
-Foreach($Folder in $Folders) {
-  $UserName = $Folder
-  $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History" 
-  if (-not (Test-Path -Path $Path)) { 
-    Write-Verbose "[!] Could not find Edge History for username: $UserName" 
-  } 
-  $Regex = '(http(|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
-  $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique 
-  $Value | ForEach-Object { 
-    $Key = $_ 
-    if ($Key -match $Site){ 
-      $Out += "$UserName Edge History $_ "
-      } 
+function edge{
+  $Folders = Get-ChildItem -Directory "C:\Users" | Where-Object {($_.Name -notlike "Public") -and ($_.Name -notlike "Administrador")}
+  $Out = ''
+  Foreach($Folder in $Folders) {
+    $UserName = $Folder
+    $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History" 
+    if (-not (Test-Path -Path $Path)) { 
+      Write-Verbose "[!] Could not find Edge History for username: $UserName" 
     } 
+    $Regex = '(http(|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+    $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique 
+    $Value | ForEach-Object { 
+      $Key = $_ 
+      if ($Key -match $Site){ 
+        $Out += "$UserName Edge History $_ "
+        } 
+      } 
+    }
   }
+  $global:execute=$Out
 }
-$global:execute=$Out
