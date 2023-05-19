9f4a74bb881efb55dc026151bee0517c7fda4ec9
josprou <34846208+josprou@users.noreply.github.com>
2023-05-19 03:47:00 +0200

    Update edge.ps1

--- modules/edge.ps1
+++ modules/edge.ps1
@@ -1,21 +1,11 @@
 $Folders = Get-ChildItem -Directory "C:\Users" | Where-Object {($_.Name -notlike "Public") -and ($_.Name -notlike "Administrador")}
 Foreach($Folder in $Folders) {
   $UserName = $Folder
-	$Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History" 
+  $Path = "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History" 
   if (-not (Test-Path -Path $Path)) { 
     Write-Verbose "[!] Could not find Edge History for username: $UserName" 
   } 
   $Regex = '(http(|s))://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
   $Value = Get-Content -Path "$Env:systemdrive\Users\$UserName\AppData\Local\Microsoft\Edge\User Data\Default\History"|Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique 
-  $Value | ForEach-Object { 
-    $Key = $_ 
-    if ($Key -match $Site){ 
-      New-Object -TypeName PSObject -Property @{ 
-        User = $UserName 
-        Browser = 'Edge' 
-        DataType = 'History' 
-        Data = $_ 
-      } 
-    } 
-  }
+  $Value
 }
