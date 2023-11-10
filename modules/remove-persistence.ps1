53703b726fdcc41156952f96e7f6155c05d03ebf
josprou <34846208+josprou@users.noreply.github.com>
2023-11-10 14:22:59 +0100

    Update remove-persistence.ps1

--- modules/remove-persistence.ps1
+++ modules/remove-persistence.ps1
@@ -1,3 +1,4 @@
 function Remove-Persistence{
   reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "OneDriveUpdate" /f
+  return @{results="Persistencia eliminada";success=$true}
 }
