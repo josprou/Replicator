9e6292175f7faa9a7bf6024c3eca7abb53dc1a90
josprou <34846208+josprou@users.noreply.github.com>
2023-11-11 13:02:24 +0100

    Update fondo.ps1

--- modules/fondo.ps1
+++ modules/fondo.ps1
@@ -1,3 +1,3 @@
 $RegKey='HKCU:\Control Panel\Desktop\'
-(New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/josprou/Replicator/main/Replicator.ps1') > $Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Microsoft\fondo.jpg
+(New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/josprou/Replicator/main/fondo.jpg') > $Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Microsoft\fondo.jpg
 Set-ItemProperty -Path $RegKey -Name 'Wallpaper' -Value "$Env:SystemDrive\Users\$Env:USERNAME\AppData\Local\Microsoft\fondo.jpg"
