ee8d1a5400a3605ed0a3cfe8d03c4e4ca01213eb
josprou <34846208+josprou@users.noreply.github.com>
2023-11-15 23:53:51 +0100

    Create explorer.psm1

---
+++ modules/explorer.psm1
@@ -0,0 +1,7 @@
+function explorer{
+    $directories = (Get-ChildItem |Select-Object Name).Name
+    foreach($dir in $directories){
+      $dirs_parsed = $dirs_parsed + "`n" + $dir
+    }
+    return @{results=$dirs_parsed;success=$true)
+}
