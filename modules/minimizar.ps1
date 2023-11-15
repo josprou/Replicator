d91b1b42d1ffe092252a818c2cffeb4f32b683ff
josprou <34846208+josprou@users.noreply.github.com>
2023-11-15 23:16:36 +0100

    Update minimizar.ps1

--- modules/minimizar.ps1
+++ modules/minimizar.ps1
@@ -1,5 +1,5 @@
 function minimizar{
   $x = New-Object -ComObject Shell.Application
   $x.minimizeall() 
-  return {result="Se han minimizado las ventanas";success=$true}
+  return @{results="Se han minimizado las ventanas";success=$true}
 }
