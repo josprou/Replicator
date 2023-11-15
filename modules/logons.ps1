153bf1cde6d4cec2671ae45b229d42cd18106ab6
josprou <34846208+josprou@users.noreply.github.com>
2023-11-15 23:22:21 +0100

    Update logons.ps1

--- modules/logons.ps1
+++ modules/logons.ps1
@@ -1,4 +1,4 @@
 function logons{
-  $logons = (Get-EventLog security | Where-Object {$_.TimeGenerated -gt '9/15/16'} | Where-Object {($_.InstanceID -eq 4634) -or ($_.InstanceID -eq 4624)} | Select-Object TimeGenerated | select -first 20).TimeGenerated
+  $logons = (Get-EventLog security | Where-Object {$_.TimeGenerated -gt '9/15/16'} | Where-Object {($_.InstanceID -eq 4634) -or ($_.InstanceID -eq 4624)} | Select-Object TimeGenerated | select -last 20).TimeGenerated
   return @{results=$logons;success=$true}
 }
